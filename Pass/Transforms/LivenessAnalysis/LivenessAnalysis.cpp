a04494b4c938dfec6816776daeacdf5d91b79808
Zoe Shang <ClockOrange@users.noreply.github.com>
2022-03-08 21:45:59 -0800

    add Liveout & modify ouput format

--- Pass/Transforms/LivenessAnalysis/LivenessAnalysis.cpp
+++ Pass/Transforms/LivenessAnalysis/LivenessAnalysis.cpp
@@ -18,7 +18,7 @@ void visitor(Function &F){
 
     // Here goes what you want to do with a pass
     
-        string func_name = "test4";
+        string func_name = "test3";
         errs() << "LivenessAnalysis: " << F.getName() << "\n";
         
         // Comment this line
@@ -35,7 +35,7 @@ void visitor(Function &F){
             std::set<string> uevar_List = {};
             std::set<string> varkill_List = {};
 
-            errs() << "------------- " << basic_block_name << " ------------ " << "\n";
+            //errs() << "------------- " << basic_block_name << " ------------ " << "\n";
 
             for (auto& inst : basic_block_var)
             {
@@ -126,43 +126,131 @@ void visitor(Function &F){
             VARKILL_table.insert(std::pair<std::string,std::set<string>>(basic_block_name,varkill_List));
 
             //errs() << " ----------- " << basic_block_name << " ---------------" << "\n";
-            errs() << "UEVAR : ";
-            std::set<std::string>::iterator itUevar;
-            for ( auto itUevar = uevar_List.begin(); itUevar != uevar_List.end(); ++itUevar  )
-            {
-                errs() << (*itUevar) << ", ";
-            } 
-            errs() <<"\n";
+            // errs() << "UEVAR : ";
+            // std::set<std::string>::iterator itUevar;
+            // for ( auto itUevar = uevar_List.begin(); itUevar != uevar_List.end(); ++itUevar  )
+            // {
+            //     errs() << (*itUevar) << ", ";
+            // } 
+            // errs() <<"\n";
 
-            errs() << "KILL : ";
-            std::set<std::string>::iterator itKill;
-            for ( auto itKill = varkill_List.begin(); itKill != varkill_List.end(); ++itKill  )
-            {
-                errs()  << (*itKill) << ", ";
-            } 
-            errs() <<"\n";
+            // errs() << "KILL : ";
+            // std::set<std::string>::iterator itKill;
+            // for ( auto itKill = varkill_List.begin(); itKill != varkill_List.end(); ++itKill  )
+            // {
+            //     errs()  << (*itKill) << ", ";
+            // } 
+            // errs() <<"\n";
         } // end all bb
         
-        errs() << " ---------------- END CHECK ---------------" << "\n" << "\n";
+        //errs() << " ---------------- END CHECK ---------------" << "\n" << "\n";
 
-        std::map<string,std::set<string>> LIVEOUT_table; 
-        bool allSame; // check all list in hash table not change
+
+       std::map<string,std::set<string>> LIVEOUT_table; 
+        for (auto& basic_block : F)
+        {
+            std::set<string> liveout_list = {};
+            std::string name = basic_block.getName().str();
+            LIVEOUT_table.insert(std::pair<std::string,std::set<string>>(name,liveout_list));
+        }
+        bool Continue = true; // check all list in hash table not change
+        while (Continue){
+            Continue = false;
+
+            for (auto& basic_block : F){
+                std::set<string> new_liveout_list = {};
+
+                //Union(Liveout(successor) - Killset(successor) + UEE(successor))
+                for (BasicBlock *succ : successors(&basic_block)){
+                    std::string succ_name = succ->getName().str();
+                    std::set<string> kill;
+                    std::set<string> uevar;
+                    std::set<string> succ_liveout;
+                    std::map<string,std::set<string>>::iterator search_succ;
+
+                    search_succ = UEVAR_table.find(succ_name);
+                    uevar = search_succ->second;
+                    search_succ = VARKILL_table.find(succ_name);
+                    kill = search_succ->second;
+                    search_succ = LIVEOUT_table.find(succ_name);
+                    succ_liveout = search_succ->second;
+
+                    std::set<string> temp;
+                    set_difference(succ_liveout.begin(),succ_liveout.end(),kill.begin(),kill.end(),inserter(temp,temp.begin()));
+                    std::set<string> temp2;
+                    set_union(temp.begin(),temp.end(),uevar.begin(),uevar.end(),inserter(temp2,temp2.begin()));
+                    std::set<string> temp3;
+                    set_union(temp2.begin(),temp2.end(),new_liveout_list.begin(),new_liveout_list.end(),inserter(temp3,temp3.begin()));
+                    new_liveout_list = temp3;
+                }
+
+                std::map<string,std::set<string>>::iterator search_block;
+                std::string name = basic_block.getName().str();
+                search_block = LIVEOUT_table.find(name);
+
+                std::set<string> old;
+                old = search_block->second;
+                LIVEOUT_table[name] = new_liveout_list;
+                
+                if (old != new_liveout_list)
+                {
+                    Continue = true;
+
+                }
+
+            }
+        }
+
+
         for (auto& basic_block : F)
         {
             // if ENTRY
             // OTHER
             // generate live out for predecessors, update
 
-            errs() << basic_block.getName() << "\n";
-            for (BasicBlock *Pred : predecessors(&basic_block)) {
+            errs() << "----- " << basic_block.getName() << " -----"<< "\n";
+            std::map<string,std::set<string>>::iterator search_bb;
+            search_bb = LIVEOUT_table.find(basic_block.getName().str());
+            std::set<string> x;
+            x = search_bb->second;
 
-                errs() << "This is pre_block check"<<"\n";
-                errs() << Pred->getName() << "\n";
-               
+
+            std::map<string,std::set<string>>::iterator uevar_bb;
+            uevar_bb = UEVAR_table.find(basic_block.getName().str());
+            std::set<string> uevar_List;
+            uevar_List = uevar_bb->second;
+            errs() << "UEVAR : ";
+            std::set<std::string>::iterator itUevar;
+            for ( auto itUevar = uevar_List.begin(); itUevar != uevar_List.end(); ++itUevar  )
+            {
+                errs() << (*itUevar) << " ";
+            } 
+            errs() <<"\n";
+
+            std::map<string,std::set<string>>::iterator kill_bb;
+            kill_bb = VARKILL_table.find(basic_block.getName().str());
+            std::set<string> varkill_List;
+            varkill_List = kill_bb->second;
+            errs() << "VARKILL : ";
+            std::set<std::string>::iterator itKill;
+            for ( auto itKill = varkill_List.begin(); itKill != varkill_List.end(); ++itKill  )
+            {
+                errs()  << (*itKill) << " ";
+            } 
+            errs() <<"\n";
+
+            errs() << "LIVEOUT : ";
+            std::set<std::string>::iterator it=x.begin();
+            while(it!=x.end()){
+                errs() << (*it) << " ";
+                it++;
             }
-            errs() << " -------- " << "\n";
+            errs() <<"\n";
+
+            //errs() << " -------- " << "\n";
          
         }
+
         
 }
 
